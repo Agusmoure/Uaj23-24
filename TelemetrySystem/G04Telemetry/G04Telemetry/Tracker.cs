b4a22510e2fd5938de327806bbaf6346f9729748
Agusmoure <amoure@ucm.es>
2024-03-27 02:29:01 +0100

    Un poco de documentacion

--- TelemetrySystem/G04Telemetry/G04Telemetry/Tracker.cs
+++ TelemetrySystem/G04Telemetry/G04Telemetry/Tracker.cs
@@ -20,7 +20,7 @@ namespace G04Telemetry
             return _instance;
         }
         /// <summary>
-        /// Inicializa el tracker
+        /// Inicializa el tracker y añadel el evento de inicio de Tracker
         /// </summary>
         /// <param name="gameName">Nombre del juego que se va a trackear</param>
         /// <param name="timeToFlush">Tiempo entre envio y envio de datos</param>
@@ -44,10 +44,19 @@ namespace G04Telemetry
         {
             return _sessionID;
         }
+        /// <summary>
+        /// Añade el evento a la cola
+        /// </summary>
+        /// <param name="e"></param>
         public void addEvent(BaseEvent e)
         {
             _events.Enqueue(e);
         }
+        /// <summary>
+        /// Elimina el primer evento de la cola si puede si no lanza excepcion
+        /// </summary>
+        /// <returns>El evento que ha sido eliminado de la cola</returns>
+        /// <exception cref="Exception"></exception>
         public BaseEvent removeEvent()
         {
             if (_events.Count > 0)
@@ -67,17 +76,69 @@ namespace G04Telemetry
         {
             return _userId;
         }
+        /// <summary>
+        /// Añade el evento de inicio de sesion
+        /// </summary>
         public void startSession()
         {
             addEvent(new SessionStartEvent(_gameName, _userId));
         }
-        public void stopSession()
+        /// <summary>
+        /// Añade ek evento de fin de sesion
+        /// </summary>
+        public void endSession()
         {
             addEvent(new SessionEndEvent());
         }
+        /// <summary>
+        /// Añade el evento de comienzo de juego
+        /// </summary>
+        public void startGame()
+        {
+            addEvent(new GameStartEvent());
+        }
+        /// <summary>
+        /// Añade el evento de final de juego
+        /// </summary>
+        public void endGame()
+        {
+            addEvent(new GameEndEvent());
+        }
+        /// <summary>
+        /// Añade el evento de inicio de nivel
+        /// </summary>
+        public void startLevel()
+        {
+            addEvent(new LevelStartEvent());
+        }
+        /// <summary>
+        /// Añade el evento de fin de nivel
+        /// </summary>
+        public void endLevel()
+        {
+            addEvent(new LevelEndEvent());
+        }
+        /// <summary>
+        /// Añade el evento de pausa
+        /// </summary>
+        public void pause()
+        {
+            addEvent(new PauseEvent());
+        }
+        /// <summary>
+        /// Añade el evento de salir de pausa(resume)
+        /// </summary>
+        public void resume()
+        {
+            addEvent(new ResumeEvent()); 
+        }
+        /// <summary>
+        /// Se añade el evento de cerrar el tracker y envia los elementos en la lista pendientes
+        /// </summary>
         public void closeTracker()
         {
             addEvent(new TrackerEndEvent());
+            //TODO hay que enviar los eventos restantes en la cola
         }
         public Queue<BaseEvent> getEvents()
         {
@@ -90,6 +151,7 @@ namespace G04Telemetry
             if( _timer > _timeToFlush)
             {
                 Console.WriteLine("FLUSH");
+                //TODO hay que enviar los eventos restantes en la cola
                 _timer = 0;
             }
 
